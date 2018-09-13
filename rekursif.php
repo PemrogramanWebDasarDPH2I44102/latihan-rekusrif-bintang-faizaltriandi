1ffad1adf731bb134f82bb14e01b47c6fee2fe77
faizaltriandi <42662893+faizaltriandi@users.noreply.github.com>
2018-09-13 08:57:50 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,41 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
+$batas = 5;
+function rekursif($i =1){
+global $batas=5;
+  echo "*";
+  $i++;
+  if($i>$batas)
     rekursif($i);
+}else{
+	echo "<br>";
+	$batas--;
+if($batas !=0){
+	rekursif();
 }
-rekursif(5);
+}
+}
+rekrusif();
+
+echo"<br>"
 ?>
+
+===============================================================================================
+<?php
+  $batas1=5;
+function rekrusif($t=1){
+	global $batas1;
+	echo $t;
+	$t++;
+	if($t<=$batas1){
+		rekrusif($t);
+	}else{
+		echo "<br>";
+		$batas1++;
+		if($batas1<=5){
+			rekrusif()
+		}
+	}
+	}
+	rekrusif();
+  ?>
